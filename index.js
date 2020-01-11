b98bbe47f31b0557c9913ed8d90de9a9411c6ffa
amfetaminka <amfetaminka4@gmail.com>
2020-01-11 23:32:02 +0300

    Сделано все, но есть проблема с лоудером

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
-let exchange = document.querySelector(".exchange");
 let btn = document.querySelector(".btn");
+let col = document.querySelector(".col");
+let bodyTable= document.querySelector(".table-body");
 let menu = document.querySelector(".dropdown-menu");
-let tbodyEl = document.querySelector(".tbody");
-function buildMoney() {
-menu.addEventListener("click", function(event){
-  let textContent = event.target.textContent;
-  btn.innerHTML = `${textContent}`;
-  exchange.innerHTML = `${textContent} Exchange rates`;
-  console.log(textContent);
-  readRate(textContent);
-});
-}
-async function readRate(textContent) {
-  try{
-    let moneyAll = await fetch(`https://api.exchangerate-api.com/v4/latest/${textContent}`);
-    let money = await moneyAll.json();
-    let arrValues = Object.values(money.rates);
-    let arrKeys = Object.keys(money.rates);
-    console.log(money);
-    buildTable(arrKeys, arrValues);
-  }
-  catch {
-    alert("error")
-  }
-}
-function buildTable(arrKeys, arrValues) {
-  console.log(arrValues, arrKeys);
-    // for (let i = 0; i < arrValues.lenght; i++) {
-    //   tbodyEl.insertAdjacentHTML('beforeend',  `<tr class="row-${i+1}">
-    //                                             <th scope="row"> ${arrKeys[i]} </th>
-    //                                             <td>${arrValues[i]}</td>
-    //                                             </tr>` 
-    //                                             );
-                                                
-    // }
-    for(let i = 0; i < arrValues.length; i++) {
-      tbodyEl.insertAdjacentHTML('beforeend', `<tr class="row-${i+1}">
-                                                  <th scope="row"> ${arrKeys[i]} </th>
-                                                  <td>${arrValues[i]}</td>
-                                              </tr>` 
-                                              );
-  }
-}
-buildMoney();
+let spinnerJs = document.querySelector('.spinner-border');
+spinnerJs.style.display = 'none';
 
+function exchangeRates() {
+    if(localStorage.length == 0) {
+        localStorage.setItem('value', 'USD');
+        let menuEl = document.querySelector(`.${localStorage.getItem('value')}`);
+        menuEl.classList.add('active');
+    } 
+    else {
+        let menuEl = document.querySelector(`.${localStorage.getItem('value')}`);
+        menuEl.classList.add('active');
+    }
+    readMoney();
+}
+
+function buildTbody(arrKeys, arrValues) {
+    btn.innerHTML = localStorage.getItem('value');
+    col.innerHTML = localStorage.getItem('value') + " Exchange rate";
+    bodyTable.innerHTML = '';
+    for(let i = 0; i < arrValues.length; i++) {
+        let tbodyTextEl =  `<tr class="row-${i+1}">
+                                <th scope="row"> ${arrValues[i]} </th>
+                                <td>${arrKeys[i]}</td>
+                            </tr>`;
+        bodyTable.insertAdjacentHTML('beforeend', tbodyTextEl);
+    };
+}
+
+async function readMoney () {
+    try {
+        loader();
+        let money = await fetch(`https://api.exchangerate-api.com/v4/latest/${localStorage.getItem('value')}`);
+        let moneys = await money.json();
+        let arrValues = Object.keys(moneys.rates);
+        let arrKeys = Object.values(moneys.rates);
+        buildTbody(arrKeys, arrValues);
+    }
+    catch {
+        alert("Sorry, check your internet connection!")
+    }
+    finally {
+        spinnerJs.remove();
+    }
+}
+
+function buildThead(event) {
+    if(event.target.textContent.length == 3) {
+        localStorage.setItem('value', event.target.textContent);
+        let menuEl = event.target;
+        for(let i = 0; i < menu.children.length; i++) {
+            let linkEl = menu.children[i];
+            linkEl.classList.remove('active');
+        }
+        menuEl.classList.add('active');
+        readMoney();
+    }
+}
+menu.addEventListener('click', buildThead);
+
+function loader() {
+    spinnerJs.style.display = 'flex';
+}
+exchangeRates();
 
